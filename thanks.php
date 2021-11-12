15fa1ac445526c7a1ab94f3bcbfb9c791c4141f9
Kirill Pushkarow <kirrrilll98@gmail.com>
2021-11-12 13:35:15 +0200

    add

---
+++ thanks.php
@@ -0,0 +1,153 @@
+<?php
+include('../config.php');
+
+//***************** Страница с завершением заказа ******************
+session_start();
+ 
+// формируем массив с товарами в заказе (если товар один - оставляйте только первый элемент массива)
+$productList = array (
+  0 => array (
+    'goodID' => 155402,
+    'quantity' => 1,
+    'price' => 75
+  )
+);
+$data = array(
+  'phone' => $_REQUEST['phone'],
+  'fio' => $_REQUEST['name'],
+  'goods' => $productList,
+  'email' => $_REQUEST['email'],
+  'domain' => 'Kir-META-Евгения Коновалова',
+   'webmasterID' => '13',
+   'utm_source'      => $_SESSION['utms']['utm_source'],  // utm_source
+    'utm_medium'      => $_SESSION['utms']['utm_medium'],  // utm_medium
+    'utm_term'        => $_SESSION['utms']['utm_term'],    // utm_term
+    'utm_content'     => $_SESSION['utms']['utm_content'], // utm_content
+    'utm_campaign'    => $_SESSION['utms']['utm_campaign'],// utm_campaign
+);
+
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'https://majorcall.leadvertex.ru/api/admin/addOrder.html?token=Irinamama97');
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+$out = curl_exec($curl);
+curl_close($curl);
+//$out – ответ сервера в формате JSON
+?>
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+
+        <meta charset="utf-8">
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '566327751306287');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=566327751306287&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+<script>
+  fbq('track', 'Lead');
+</script>
+
+        <title>Поздравляем! Ваш заказ принят!</title><script>fbq('track', 'Lead');</script>
+        <style type="text/css">
+            body {
+                line-height: 1;
+                height: 100%;
+                font-family: Arial;
+                font-size: 15px;
+                color: #313e47;
+                width: 100%;
+                height: 100%;
+                padding: 0;
+                margin: 0;
+                background: url('bg-ok.png');
+            }
+            h2 {
+                margin: 0;
+                padding: 0;
+                font-size: 36px;
+                line-height: 44px;
+                color: #313e47;
+                text-align: center;
+                font-weight: bold;
+            }
+            a {
+                color: #69B9FF;
+            }
+            .list_info li span {
+                width: 150px;
+                display: inline-block;
+                font-weight: bold;
+                font-style: normal;
+            }
+            .list_info {
+               text-align: left;
+               display: inline-block;
+               list-style: none;
+               margin-top: -10px;
+               margin-bottom: -11px;
+            }
+            .list_info li {
+                margin: 11px 0px;
+            }
+            .fail {
+                margin: 10px 0 20px 0px;
+                text-align: center;
+            }
+            .email {
+                position: relative;
+                text-align: center;
+                margin-top: 40px;
+            }
+            .email input {
+                height: 30px;
+                width: 200px;
+                font-size: 14px;
+                padding-right: 10px;
+                padding-left: 10px;
+                outline: none;
+                -webkit-border-radius: 5px;
+                -moz-border-radius: 5px;
+                border-radius: 5px;
+                border: 1px solid #B6B6B6;
+                margin-bottom: 10px;
+            }
+            .block_success {
+                max-width: 960px;
+                padding: 70px 30px 70px 30px;
+                margin: -50px auto;
+            }
+            .success {
+                text-align: center;
+            }
+        </style> 
+
+		
+    </head>
+    <body>
+
+        <div class="block_success">					
+            <h2 style="text-transform: uppercase;">Поздравляем! Ваш заказ принят!</h2>
+            <p class="success">
+                В ближайшее время с вами свяжется оператор для подтверждения заказа. Пожалуйста, включите ваш контактный телефон.
+            </p>
+            <p class="fail success">Если вы ошиблись при заполнени формы, то, пожалуйста, <a href="javascript: history.back(-1);">заполните заявку еще раз</a></p>
+
+			
+        </div>
+ </body>
+</html>
